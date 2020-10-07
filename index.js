7d5430fd1da8351902ff453d1dd17cd7237d2229
Petar Cale <usavatreni@gmail.com>
2020-10-07 02:05:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
     return "I can't hear you!";
-  } else 
+  } else
     if (string === string.toUpperCase()) {
       return "YES INDEED!";
-    } else 
+    } else
      if (string === "I love you, Grandma") {
        return "I love you, too."
      }
