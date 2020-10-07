64e594a5d43f0cc8a9b08cc3d781106f75f7c861
Petar Cale <usavatreni@gmail.com>
2020-10-07 02:15:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ function sayHiToGrandma(string) {
     if (string === string.toUpperCase()) {
       return "YES INDEED!";
     } else if (string === "I love you, Grandma") {
-       return "I love you, too."
+       return 'I love you, too.';
      }
 }
